4b6a03774359b8db64e0892df11e102113b99509
AliceHeeyeon <270275774@yoobeestudent.ac.nz>
2023-09-11 08:58:04 +1200

    backend-product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,13 +5,25 @@ const router = express.Router()
 // import controller
 const {
     getProducts,
-    createProduct
+    getProduct,
+    createProduct,
+    deleteProduct,
+    updateProduct
 } = require('../controllers/productController')
 
 // GET all products
 router.get('/', getProducts)
 
+// GET a single product
+router.get('/:id', getProduct)
+
 // POST products
 router.post('/', createProduct)
+
+// DELETE products
+router.delete('/:id', deleteProduct)
+
+// UPDATE product
+router.patch('/:id', updateProduct)
 
 module.exports = router
