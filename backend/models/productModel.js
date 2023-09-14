9e40281d62ef7cfd2d73d50f8dcb1804e8e7a8e2
ntoni596 <ntoni596@gmail.com>
2023-09-14 13:17:18 +1200

    changes

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,38 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
+const productSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     materials: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     comments: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
-}, {timestamps: true})
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model("Product", productSchema);
