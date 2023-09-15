dd2ff965990108f3176ce55b4a4aaacd30c5d480
ntoni596 <ntoni596@gmail.com>
2023-09-15 17:19:58 +1200

    form updates

---
+++ frontend/src/components/MultiSelectDropdown.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const MultiSelectDropdown = ({ options, selectedItems, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    onChange(option);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className="multi-select-dropdown" ref={dropdownRef}>
+      <div className="dropdown-header" onClick={toggleDropdown}>
+        {selectedItems.length === 0
+          ? "Select Materials"
+          : selectedItems.join(", ")}
+        <span className={`chevron ${isOpen ? "chevron-up" : "chevron-down"}`}>
+          &#x25BC;
+        </span>
+      </div>
+      {isOpen && (
+        <div className="dropdown-options">
+          {options.map((option) => (
+            <div
+              key={option}
+              className={`dropdown-option ${
+                selectedItems.includes(option) ? "selected" : ""
+              }`}
+              onClick={() => handleOptionClick(option)}>
+              {selectedItems.includes(option) && (
+                <span className="dot">&#8226;</span>
+              )}
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MultiSelectDropdown;
