b22811e9cce167035b78e7b84c12d3be52bba1f5
Yassoori <yasserasaeed@gmail.com>
2023-09-17 14:55:22 +1200

    sunday Co-authored-by: Ntoni596 <Ntoni596@users.noreply.github.com>

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -5,22 +5,18 @@ const ProductCard = ({ product }) => {
   const icons = useIcons();
 
   return (
-    <div className="product-grid">
-      <div className="product-card">
-        <React.Suspense fallback={<div>Loading Icons...</div>}>
-          <icons.HeartIcon className="heart-icon" />
-        </React.Suspense>
-        <img
-          className="product-image"
-          src={product.image}
-          alt="Product Image"
-        />
-        <div className="product-name">{product.title}</div>
-        <div className="product-description">{product.description}</div>
-        <div className="product-material">{product.materials}</div>
-        <div className="product-price">${product.price} Tax incl.</div>
-      </div>
+    // <div className="product-grid">
+    <div className="product-card">
+      <React.Suspense fallback={<div>Loading Icons...</div>}>
+        <icons.HeartIcon className="heart-icon" />
+      </React.Suspense>
+      <img className="product-image" src={product.image} alt="Product Image" />
+      <div className="product-name">{product.title}</div>
+      <div className="product-description">{product.description}</div>
+      <div className="product-material">{product.materials}</div>
+      <div className="product-price">${product.price} Tax incl.</div>
     </div>
+    // </div>
   );
 };
 
