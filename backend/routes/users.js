a88a90c2ef81ef1c772d601b8406e01f4b49a3a7
ntoni596 <ntoni596@gmail.com>
2023-09-21 15:50:38 +1200

    Cart Delete Function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,7 @@ const {
   addToCart,
   fetchCart,
   removeFromWishlist,
+  removeCart,
 } = require("../controllers/userController");
 
 // Login
@@ -23,7 +24,7 @@ router.get("/:userId/wishlist/products", fetchWishlist);
 // Add to Cart
 router.post("/:userId/cart/:productId", addToCart);
 router.get("/:userId/cart/products", fetchCart);
-
+router.delete("/:userId/cart/:productId", removeCart);
 router.delete("/:userId/wishlist/:productId", removeFromWishlist);
 
 module.exports = router;
