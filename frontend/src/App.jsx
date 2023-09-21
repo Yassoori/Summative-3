d8d43eca7524e270d0255325948aa1e457b1d6d8
AliceHeeyeon <270275774@yoobeestudent.ac.nz>
2023-09-22 09:04:53 +1200

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,6 +62,7 @@ function App() {
                 {isLoginVisible && (
                   <LoginSignup onClose={handleLoginModalClose} />
                 )}
+                <Footer/>
               </ProductsContextProvider>
             </IconContextProvider>
           </WishlistProvider>
