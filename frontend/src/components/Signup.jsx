b04037356671a4f9399ef28be1568178a424c3a9
AliceHeeyeon <270275774@yoobeestudent.ac.nz>
2023-09-22 10:55:57 +1200

    signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
+import { useLoginModalContext } from "../hooks/useLoginModalContext";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
@@ -7,6 +8,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   // set default isvendor to false
   const [isvendor, setIsvendor] = useState("false");
+  const { dispatch } = useLoginModalContext();
 
   // bring in signup function, loading state, error from our hook:
   const { signup, error, isLoading } = useSignup();
@@ -15,9 +17,13 @@ const Signup = () => {
     e.preventDefault();
 
     await signup(username, email, password, isvendor);
+
+    const userData = localStorage.getItem("user");
+    if (userData) {
+      dispatch({ type: "SIGNUP_CLOSE" });
+    }
   };
 
-  // const Signup = () => {
   return (
     <div className="signup">
       <form className="signup-form" onSubmit={handleSubmit}>
